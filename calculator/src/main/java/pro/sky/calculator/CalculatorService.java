aaf5f001ec76169354b45aeafe40a2b2a01726cc
ZyxpaKarymova <z_karimova@list.ru>
2024-01-22 02:50:14 +0600

    Commit

--- calculator/src/main/java/pro/sky/calculator/CalculatorService.java
+++ calculator/src/main/java/pro/sky/calculator/CalculatorService.java
@@ -29,7 +29,7 @@ public class CalculatorService {
     }
 
     public String division(int num1, int num2) {
-        if ((num2 == 0)){
+        if (num2 == 0){
             return "Division by zero";
         }
         else{
